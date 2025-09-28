7c41c5393bb5d62f117796bd4c494912bda899e7
ConorOnSol <viraldoss06@gmail.com>
2025-09-28 18:57:21 +0100

    Update constants.ts

--- src/games/Roulette/constants.ts
+++ src/games/Roulette/constants.ts
@@ -1,30 +1,95 @@
-export const CHIPS = [1, 5, 25, 100, 500]
-export const NUMBERS = 37 // 0-36
+export { default as SOUND_CHIP } from './chip.mp3'
+export { default as SOUND_LOSE } from './lose.mp3'
+export { default as SOUND_PLAY } from './play.mp3'
+export { default as SOUND_WIN } from './win.mp3'
 
-export const SOUND_CHIP = 'https://raw.githubusercontent.com/gamba-labs/gamba/main/apps/play/public/chip.wav'
-export const SOUND_WIN = 'https://raw.githubusercontent.com/gamba-labs/gamba/main/apps/play/public/win.wav'
-export const SOUND_LOSE = 'https://raw.githubusercontent.com/gamba-labs/gamba/main/apps/play/public/lose.wav'
-export const SOUND_PLAY = 'https://raw.githubusercontent.com/gamba-labs/gamba/main/apps/play/public/play.wav'
+export const CHIPS = [1, 2, 10, 50]
+export const NUMBERS = 18
+export const NUMBER_COLUMNS = Math.ceil(NUMBERS / 3)
 
-export const tableLayout: Record<string, { label: string; numbers: number[]; row: number; column: string; color: 'red' | 'black' | 'green' }> = {
-  '0': { label: '0', numbers: [0], row: 1, column: '1', color: 'green' },
-  
-  '1': { label: '1', numbers: [1], row: 2, column: '1', color: 'red' },
-  '2': { label: '2', numbers: [2], row: 2, column: '2', color: 'black' },
-  '3': { label: '3', numbers: [3], row: 2, column: '3', color: 'red' },
-  
-  '4': { label: '1st', numbers: [1, 2, 3], row: 3, column: '1', color: 'black' },
-  '5': { label: '2nd', numbers: [4, 5, 6], row: 3, column: '2', color: 'red' },
-  '6': { label: '3rd', numbers: [7, 8, 9], row: 3, column: '3', color: 'black' },
-  
-  '7': { label: '1-12', numbers: Array.from({ length: 12 }, (_, i) => i + 1), row: 4, column: '1', color: 'red' },
-  '8': { label: '2-12', numbers: Array.from({ length: 12 }, (_, i) => i + 13), row: 4, column: '2', color: 'black' },
-  '9': { label: '3-12', numbers: Array.from({ length: 12 }, (_, i) => i + 25), row: 4, column: '3', color: 'red' },
-  
-  '10': { label: '1-18', numbers: Array.from({ length: 18 }, (_, i) => i + 1), row: 5, column: '1', color: 'black' },
-  '11': { label: 'Even', numbers: Array.from({ length: 18 }, (_, i) => (i + 1) * 2), row: 5, column: '2', color: 'red' },
-  '12': { label: 'Red', numbers: [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36], row: 5, column: '3', color: 'red' },
-  '13': { label: 'Black', numbers: [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35], row: 5, column: '4', color: 'black' },
-  '14': { label: 'Odd', numbers: Array.from({ length: 18 }, (_, i) => i * 2 + 1), row: 5, column: '5', color: 'black' },
-  '15': { label: '19-36', numbers: Array.from({ length: 18 }, (_, i) => i + 19), row: 5, column: '6', color: 'red' },
+const RED_NUMBERS = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+
+const isRed = (number: number) => RED_NUMBERS.includes(number % (RED_NUMBERS.length + 1))
+
+const getRow = (index: number) => (3 - index % 3)
+
+const allNumbers = Array.from({ length: NUMBERS }).map((_, i) => i + 1)
+
+const makeSquare = (
+  label: string,
+  numberFilter: (number: number) => boolean,
+  [column, row]: [number, number],
+): TableSquare => {
+  const numbers = allNumbers.filter(numberFilter)
+  return { label, numbers, row, column }
+}
+
+interface TableSquare {
+  label: string
+  numbers: number[]
+  row: number
+  column: number
+  color?: 'red' | 'black'
+}
+
+type TableLayout = Record<string, TableSquare>
+
+const numbersLayout = Array.from({ length: NUMBERS }).reduce<TableLayout>((prev, _, index) => ({
+  ...prev,
+  [index + 1]: {
+    label: String(index + 1),
+    numbers: [String(index + 1)],
+    row: getRow(index),
+    column: 1 + Math.floor(index / 3),
+    color: isRed(index + 1) ? 'red' : 'black',
+  },
+}), {})
+
+export const tableLayout: TableLayout = {
+  ...numbersLayout,
+  firstHalf: makeSquare(
+    '<' + Math.floor(NUMBERS / 2 + 1),
+    (number) => number <= NUMBERS / 2,
+    [1, 4],
+  ),
+  even: makeSquare(
+    'Even',
+    (number) => number % 2 === 0,
+    [2, 4],
+  ),
+  red: makeSquare(
+    'Red',
+    (number) => isRed(number),
+    [3, 4],
+  ),
+  black: makeSquare(
+    'Black',
+    (number) => !isRed(number),
+    [4, 4],
+  ),
+  odd: makeSquare(
+    'Odd',
+    (number) => number % 2 === 1,
+    [5, 4],
+  ),
+  secondHalf: makeSquare(
+    '>' + Math.floor(NUMBERS / 2),
+    (number) => number > NUMBERS / 2,
+    [6, 4],
+  ),
+  row1: makeSquare(
+    '2:1',
+    (number) => getRow(number - 1) === 1,
+    [NUMBER_COLUMNS + 1, 1],
+  ),
+  row2: makeSquare(
+    '2:1',
+    (number) => getRow(number - 1) === 2,
+    [NUMBER_COLUMNS + 1, 2],
+  ),
+  row3: makeSquare(
+    '2:1',
+    (number) => getRow(number - 1) === 3,
+    [NUMBER_COLUMNS + 1, 3],
+  ),
 }
