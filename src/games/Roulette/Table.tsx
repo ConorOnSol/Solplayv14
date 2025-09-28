fa26f098cb2c44bd87ea35dd80bcf978f42a1f76
ConorOnSol <viraldoss06@gmail.com>
2025-09-28 18:56:59 +0100

    Update Table.tsx

--- src/games/Roulette/Table.tsx
+++ src/games/Roulette/Table.tsx
@@ -38,10 +38,7 @@ export function Table() {
                   remove(id)
                 }
               }}
-              onContextMenu={(e) => {
-                e.preventDefault()
-                remove(id)
-              }}
+              onContextMenu={() => remove(id)}
               style={{
                 gridRow: square.row,
                 gridColumn: square.column,
